782988c89ce9dc56b44a73281c1092c0b42c9cfd
Junaidhassan99 <61021884+Junaidhassan99@users.noreply.github.com>
2022-03-19 19:07:29 +0500

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <h1>project is running</h1>;
+ return <h1>project is running</h1>;
 };
 
 export default Home;
