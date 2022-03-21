e81696e572297f341f829aee8e4098a9feacfd37
Junaidhassan99 <61021884+Junaidhassan99@users.noreply.github.com>
2022-03-21 16:45:32 +0500

    .

--- components/CalculatorButton.tsx
+++ components/CalculatorButton.tsx
@@ -1,35 +1,43 @@
 import { useAppSelector, useAppDispatch } from "../store/hooks";
 
-import { ButtonKeyType, onShiftClick } from "../store/calculator-slice";
+import {
+  ButtonKeyGroupType,
+  ButtonKeyType,
+  onShiftClick,
+} from "../store/calculator-slice";
 
 import classes from "../styles/calculator-home-screen.module.css";
+import { Fragment } from "react";
 
 const CalclatorButton: React.FC<{
-  buttonKey: ButtonKeyType;
-}> = ({ children, buttonKey }) => {
+  buttonKeyGroup: ButtonKeyGroupType;
+}> = ({ children, buttonKeyGroup }) => {
   const calculatorDispatch = useAppDispatch();
 
-  if (buttonKey.id === "k-d") {
-    return <div></div>;
-  } else if (buttonKey.isVisible) {
-    return (
-      <button
-        className={
-          buttonKey.isBigSize === true ? classes["button-item"] : undefined
-        }
-        onClick={() => {
-          if (buttonKey.id === "k-46") {
-            calculatorDispatch(onShiftClick());
-          }
-        }}
-      >
-        {buttonKey.text}
-        {/* {`${buttonKey.text} - ${buttonKey.id}`} */}
-      </button>
-    );
-  } else {
-    return null;
-  }
+  const mId = buttonKeyGroup.id;
+
+  return (
+    <Fragment>
+      {buttonKeyGroup.value.map((item) => {
+        return item.isVisible ? (
+          <button
+            className={`${
+              buttonKeyGroup.value.length === 1
+                ? classes["button-item"]
+                : undefined
+            } ${mId === "k-d" ? classes["dummy-item"] : undefined}`}
+            onClick={() => {
+              if (item.text === "shift") {
+                calculatorDispatch(onShiftClick());
+              }
+            }}
+          >
+            {item.text}
+          </button>
+        ) : null;
+      })}
+    </Fragment>
+  );
 };
 
 export default CalclatorButton;
