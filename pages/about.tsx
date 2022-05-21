5d1c529d3d6ad4f4b8fc439e9e2c872f30d23cd1
Junaidhassan99 <61021884+Junaidhassan99@users.noreply.github.com>
2022-05-21 11:43:33 +0500

    about screen completed

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,38 @@
-const DisplayAboutText: React.FC<{
+const AboutTextItem: React.FC<{
   title: string;
-  description?: string;
-}> = ({ children, title, description }) => {
-  return <div></div>;
+  description: string;
+  path?: string;
+}> = ({ children, title, description, path }) => {
+  return (
+    <div className="about-text-item">
+      <div>{`${title}:`}</div>
+      <div className="about-item-spacer"></div>
+      {path !== undefined ? (
+        <a href={path}>{description}</a>
+      ) : (
+        <div>{description}</div>
+      )}
+    </div>
+  );
 };
 
 const AboutPage: React.FC<{}> = ({ children }) => {
-  return <div></div>;
+  return (
+    <div className="about-container">
+      <AboutTextItem title="App Name" description="Scientific Calculator" />
+      <AboutTextItem title="Developed Using" description="Next JS" />
+      <AboutTextItem
+        title="Source Code"
+        description="View on Github"
+        path="https://github.com/Junaidhassan99/next-calculator"
+      />
+      <AboutTextItem
+        title="Developed By"
+        description="Junaid Hassan"
+        path="mailto:junaidhassan2211@gmail.com"
+      />
+    </div>
+  );
 };
 
 export default AboutPage;
